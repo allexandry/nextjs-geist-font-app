bc8a8626d88a76eadf706e7a4b6b92ebfb2ff2fd
Koffivi Sehyram DJOKOUI <busidjok@gmail.com>
2025-07-10 11:47:25 +0100

    Add .next/static/chunks/src_app_school-registration_page_tsx_5ee32bad._.js 1752144444287

---
+++ .next/static/chunks/src_app_school-registration_page_tsx_5ee32bad._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_862b0c79._.js",
+  "static/chunks/node_modules_1fe4edd4._.js"
+],
+    source: "dynamic"
+});
